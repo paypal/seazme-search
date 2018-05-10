00268c2364f741ec4a82e4e9436818b85d25b2a8
AndyL <andy.innersource@gmail.com>
2018-05-09 19:18:57 -0700

    dust2ejs

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//TODO: why dev and prod middleware "view engines" use "dust" and "js" and there is copy paste?
 module.exports = function IndexModel() {
     return {
         name: 'index'
