c8f555954e3dd4b8ad1a3fa754ae4941fa7fbd4a
AndyL <andy.innersource@gmail.com>
2018-05-09 19:20:14 -0700

    react mininal code

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/.build/js',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: '.build/js'
+  }
+};
